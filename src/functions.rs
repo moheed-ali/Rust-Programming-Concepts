3f55ff1b7dcba0c971a560436eae3d349fb3b0a1
moheed-ali <moheedalikayani@outlook.com>
2024-04-03 10:46:24 +0200

    Adding file for Control Flow

---
+++ src/functions.rs
@@ -0,0 +1,32 @@
+fn main () {
+    another_function(5);
+    print_labeled_measurement(5, 'h');
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The Value of Y is: {y}");
+
+    let a = plus_one(5);
+
+    println!("The value of A is: {a}");
+
+}
+
+fn another_function(x: i32){
+    println!("The Value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The Measurement is: {value}{unit_label}");
+}
+
+fn five () -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}

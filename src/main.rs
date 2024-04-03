ce70a7148ffe36f039ff4c2b7ef86880d0f674a6
moheed-ali <moheedalikayani@outlook.com>
2024-04-03 11:06:28 +0200

    Multiple if else

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 fn main () {
 
-    let num = 2;
+    let num = 6;
 
-    if num < 5 {
-        println!("Condition was true");
-    } else{
-        println!("Condition was false");
+    // if num < 5 {
+    //     println!("Condition was true");
+    // } else{
+    //     println!("Condition was false");
+    // }
+
+    if num % 4 == 0 {
+        println!("Number is divisible by 4");
+    } else if num % 3 == 0{
+        println!("Number is divisible by 3");
+    } else if num % 2 == 0{
+        println!("Number is divisible by 2");
+    } else {
+        println!("Number {num} is not divisible by 4, 3 and 2");
     }
 }
 

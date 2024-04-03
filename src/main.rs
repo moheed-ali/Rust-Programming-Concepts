6305ca86a7d88010225e705fdc833c2541bab057
moheed-ali <moheedalikayani@outlook.com>
2024-04-03 11:17:25 +0200

    Simple Loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 fn main () {
 
-    let num = 6;
+    let mut counter = 0; 
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10{
+            break counter * 2;
+        }
+    };
+
+    println!("The Value of Counter is: {result}");
+
+    // let num = 6;
 
     // if num < 5 {
     //     println!("Condition was true");
@@ -8,14 +20,19 @@ fn main () {
     //     println!("Condition was false");
     // }
 
-    if num % 4 == 0 {
-        println!("Number is divisible by 4");
-    } else if num % 3 == 0{
-        println!("Number is divisible by 3");
-    } else if num % 2 == 0{
-        println!("Number is divisible by 2");
-    } else {
-        println!("Number {num} is not divisible by 4, 3 and 2");
-    }
+    // if num % 4 == 0 {
+    //     println!("Number is divisible by 4");
+    // } else if num % 3 == 0{
+    //     println!("Number is divisible by 3");
+    // } else if num % 2 == 0{
+    //     println!("Number is divisible by 2");
+    // } else {
+    //     println!("Number {num} is not divisible by 4, 3 and 2");
+    // }
+
+    // let condition = true;
+    // let number = if condition {5} else {6};
+
+    // println!("The value of number is: {number}");
 }
 

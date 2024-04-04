3091b7f8e59cb41af937f34f9953391384ea58b5
moheed-ali <moheedalikayani@outlook.com>
2024-04-04 14:16:18 +0200

    Bind Values Pattern

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,36 @@
+#[derive(Debug)] 
+
+enum UsState {
+    Washington,
+    Alabama,
+    New_York,
+    San_Francisco,
+    Las_Vagas,
+    Alaska,
+    // --snip--
+}
+
 enum Coin{
     penny, 
     Nickel,
     Dime,
-    Quater,
+    Quater(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8{
     match coin{
-        Coin::penny=> {
-            println!("Lucky penny!");
-            1
-        }
+        Coin::penny=> 1,
         Coin::Nickel=> 5,
         Coin::Dime=> 10,
-        Coin::Quater=> 25,
+        Coin::Quater(state)=> {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
 
 fn main () {
     println!("Enum Match");
-    let money = value_in_cents(Coin::penny);
+    let money = value_in_cents(Coin::Quater(UsState::New_York));
     println!("The Money is : {money}");
 }
